020e368cc07a59c6f15fa2676a5d085484ee6e47
Siddhant <bhandarysid@gmail.com>
2023-11-25 13:18:33 +0530

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
+
 
 function App() {
+  const [name,setName] = useState("")
+  const handleClick = async ()=>
+  {
+
+    const settings = {
+      method: "GET",
+      };
+
+    try {
+      const fetchResponse = await fetch(
+       " http://localhost:1337/api/data",
+        settings
+      );
+      const res = await fetchResponse.json();
+      if (fetchResponse.status === 200) {
+       
+        alert(res.data)
+
+      } else {
+       alert(res.message)
+      }
+    } catch (error) {
+      alert("smthng went wsfdasdf")
+    }
+
+
+
+  }
+
+
+  const handleSubmit = async ()=>
+  {
+
+    const settings = {
+      method: "POST",
+      body: JSON.stringify({
+        name:"siddhant"
+      }),
+
+      };
+
+    try {
+      const fetchResponse = await fetch(
+       " http://localhost:1337/api/submitData",
+        settings
+      );
+      const res = await fetchResponse.json();
+      if (fetchResponse.status === 200) {
+       
+        alert(res.message)
+
+      } else {
+       alert(res.message)
+      }
+    } catch (error) {
+      alert("smthng went wsfdasdf")
+    }
+
+
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Form</h1>
+    <input type="text" placeholder="name" />
+    <button onClick={()=>handleSubmit()}>Submit</button>
+    
+    <button style={{margin:"10px"}} onClick={()=>handleClick()} >Click me</button>
+    
     </div>
   );
 }
