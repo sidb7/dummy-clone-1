f946a4261f8be89670257c95e456e94defbd599d
pranav nair <pranavpn7@gmail.com>
2023-11-25 13:25:21 +0530

    header application

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
-
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const [name,setName] = useState("")
-  const handleClick = async ()=>
-  {
-
+  const [name, setName] = useState("");
+  const handleClick = async () => {
     const settings = {
       method: "GET",
-      };
+    };
 
     try {
       const fetchResponse = await fetch(
-       " http://localhost:1337/api/data",
+        " http://localhost:1337/api/data",
         settings
       );
       const res = await fetchResponse.json();
       if (fetchResponse.status === 200) {
-       
-        alert(res.data)
-
+        alert(res.data);
       } else {
-       alert(res.message)
+        alert(res.message);
       }
     } catch (error) {
-      alert("smthng went wsfdasdf")
+      alert("smthng went wsfdasdf");
     }
+  };
 
-
-
-  }
-
-
-  const handleSubmit = async ()=>
-  {
-
+  const handleSubmit = async () => {
     const settings = {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({
-        name:"siddhant"
+        name: "siddhant",
       }),
-
-      };
+    };
 
     try {
       const fetchResponse = await fetch(
-       " http://localhost:1337/api/submitData",
+        " http://localhost:1337/api/submitData",
         settings
       );
       const res = await fetchResponse.json();
       if (fetchResponse.status === 200) {
-       
-        alert(res.message)
-
+        alert(res.message);
       } else {
-       alert(res.message)
+        alert(res.message);
       }
     } catch (error) {
-      alert("smthng went wsfdasdf")
+      alert("smthng went wsfdasdf");
     }
-
-
-
-  }
+  };
 
   return (
     <div className="App">
       <h1>Form</h1>
-    <input type="text" placeholder="name" />
-    <button onClick={()=>handleSubmit()}>Submit</button>
-    
-    <button style={{margin:"10px"}} onClick={()=>handleClick()} >Click me</button>
-    
+      <input type="text" placeholder="name" />
+      <button onClick={() => handleSubmit()}>Submit</button>
+
+      <button style={{ margin: "10px" }} onClick={() => handleClick()}>
+        Click me
+      </button>
     </div>
   );
 }
